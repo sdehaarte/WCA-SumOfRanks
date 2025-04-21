6e321ea6d76912b12b9215202cbec5bf26df59d6
Andrew Chuang Saladin <andrewchuangsaladin@gmail.com>
2025-04-21 09:43:01 -0400

    Added node.js express server

---
+++ server/express.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { exec } = require('node:child_process');
+
+const app = express();
+const port = 3000;
+app.use(express.static('../public'));
+
+app.get('/api/rankings', (req, res) => {
+  const sort = req.query.sort || 'merge';
+  exec(`../src/build/wca_sor --mode sort --sort ${sort}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send(stderr);
+    }
+    res.json(JSON.parse(stdout));
+  });
+});
+
+app.listen(port, () => {
+    console.log(`Running on: http://localhost:${port}`)
+});
